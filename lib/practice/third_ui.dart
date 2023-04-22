2691a945894b4932c7123c164f92dd6fb28b5d9d
milan maniya <milanmaniya2035@gmail.com>
2023-04-22 23:05:27 +0530

    third ui completed

---
+++ lib/practice/third_ui.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+
+class ThirdUi extends StatelessWidget {
+  const ThirdUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 350,
+                    width: 70,
+                    decoration: const BoxDecoration(color: Colors.white),
+                  ),
+                  Container(
+                    height: 230,
+                    width: 70,
+                    decoration: const BoxDecoration(color: Colors.blue),
+                  ),
+                  Container(
+                    height: 203,
+                    width: 70,
+                    decoration: const BoxDecoration(color: Colors.deepOrange),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 450,
+                    width: 70,
+                    decoration: const BoxDecoration(color: Colors.yellow),
+                  ),
+                  Container(
+                    height: 333,
+                    width: 70,
+                    decoration: const BoxDecoration(color: Colors.green),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 260,
+                    width: 70,
+                    decoration: const BoxDecoration(color: Colors.white),
+                  ),
+                  Container(
+                    height: 353,
+                    width: 70,
+                    decoration: const BoxDecoration(color: Colors.grey),
+                  ),
+                  Container(
+                    height: 170,
+                    width: 70,
+                    decoration: const BoxDecoration(color: Colors.pink),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 80,
+                    width: 290,
+                    decoration: const BoxDecoration(color: Colors.blue),
+                  ),
+                  Container(
+                    height: 80,
+                    width: 290,
+                    decoration: const BoxDecoration(color: Colors.yellow),
+                  ),
+                  Container(
+                    height: 80,
+                    width: 290,
+                    decoration: const BoxDecoration(color: Colors.green),
+                  ),
+                  Container(
+                    height: 80,
+                    width: 290,
+                    decoration: const BoxDecoration(color: Colors.red),
+                  ),
+                  Container(
+                    height: 80,
+                    width: 290,
+                    decoration: const BoxDecoration(color: Colors.blue),
+                  ),
+                  Container(
+                    height: 80,
+                    width: 290,
+                    decoration: const BoxDecoration(color: Colors.yellow),
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 120,
+                            decoration:
+                                const BoxDecoration(color: Colors.deepPurple),
+                          ),
+                          Container(
+                            height: 50,
+                            width: 120,
+                            decoration:
+                                const BoxDecoration(color: Colors.deepOrange),
+                          ),
+                          Container(
+                            height: 50,
+                            width: 120,
+                            decoration: const BoxDecoration(color: Colors.blue),
+                          ),
+                          Container(
+                            height: 153,
+                            width: 120,
+                            decoration: const BoxDecoration(
+                                color: Color.fromARGB(255, 121, 238, 125)),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 40,
+                            decoration: const BoxDecoration(color: Colors.grey),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: const BoxDecoration(color: Colors.pink),
+                          ),
+                          Container(
+                            height: 25,
+                            width: 40,
+                            decoration:
+                                const BoxDecoration(color: Colors.black),
+                          ),
+                          Container(
+                            height: 25,
+                            width: 40,
+                            decoration:
+                                const BoxDecoration(color: Colors.green),
+                          ),
+                          Container(
+                            height: 153,
+                            width: 40,
+                            decoration: const BoxDecoration(color: Colors.blue),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 40,
+                            decoration: const BoxDecoration(color: Colors.grey),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration:
+                                const BoxDecoration(color: Colors.orange),
+                          ),
+                          Container(
+                            height: 25,
+                            width: 40,
+                            decoration:
+                                const BoxDecoration(color: Colors.green),
+                          ),
+                          Container(
+                            height: 25,
+                            width: 40,
+                            decoration:
+                                const BoxDecoration(color: Colors.black),
+                          ),
+                          Container(
+                            height: 153,
+                            width: 40,
+                            decoration:
+                                const BoxDecoration(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            height: 42,
+                            width: 90,
+                            decoration: const BoxDecoration(color: Colors.pink),
+                          ),
+                          Container(
+                            height: 35,
+                            width: 90,
+                            decoration: const BoxDecoration(
+                                color: Color.fromARGB(255, 114, 232, 118)),
+                          ),
+                          Container(
+                            height: 45,
+                            width: 90,
+                            decoration: const BoxDecoration(
+                                color: Color.fromARGB(255, 236, 177, 88)),
+                          ),
+                          Container(
+                            height: 65,
+                            width: 90,
+                            decoration: const BoxDecoration(color: Colors.grey),
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                height: 117,
+                                width: 45,
+                                decoration: const BoxDecoration(
+                                    color: Colors.deepOrange),
+                              ),
+                              Container(
+                                height: 117,
+                                width: 45,
+                                decoration: const BoxDecoration(
+                                    color: Color.fromARGB(255, 5, 116, 9)),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
