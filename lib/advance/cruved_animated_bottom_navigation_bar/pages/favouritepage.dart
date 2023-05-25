960d0025d6ba2b1fe77073ee85b4530b6185a5e3
milanmaniya <milanmaniya981@gmail.com>
2023-05-25 20:41:13 +0530

    done

--- lib/advance/cruved_animated_bottom_navigation_bar/pages/favouritepage.dart
+++ lib/advance/cruved_animated_bottom_navigation_bar/pages/favouritepage.dart
@@ -1,30 +1,16 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:my_project/advance/cruved_animated_bottom_navigation_bar/navigation_key.dart';
 
-class FavouritePage extends StatefulWidget {
+class FavouritePage extends StatelessWidget {
   const FavouritePage({super.key});
 
-  @override
-  State<FavouritePage> createState() => _FavouritePageState();
-}
-
-class _FavouritePageState extends State<FavouritePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple.shade400,
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            final CurvedNavigationBarState navigation =
-                navigationKey.getKey().currentState;
-
-            setState(() {
-              navigation.setPage(0);
-            });
-          },
-          child: const Text("Continue to home page"),
+      body: const Center(
+        child: Text(
+          "Favourite Page",
+          style: TextStyle(fontSize: 25),
         ),
       ),
     );
