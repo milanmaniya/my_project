7db71254dbe0c1671fc4b81d2a308cfc615eee21
milanmaniya <milanmaniya981@gmail.com>
2023-06-05 19:28:04 +0530

    done

---
+++ lib/practice/Ui7/pages/call_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/list_of_map_demo/row_data_whatsapp.dart';
+
+class CallPage extends StatelessWidget {
+  const CallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          children: List.generate(
+        myDetails.length,
+        (index) => ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(myDetails[index]['url']),
+            radius: 18,
+          ),
+          title: Text(myDetails[index]['name']),
+          subtitle: Text(myDetails[index]['message']),
+          trailing: Text(myDetails[index]['date']),
+        ),
+      )),
+    );
+  }
+}
--- lib/practice/Ui7/whatsapp_list_of_map.dart
+++ lib/practice/Ui7/whatsapp_list_of_map.dart
@@ -1,74 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:my_project/list_of_map_demo/row_data_whatsapp.dart';
+import 'package:my_project/practice/Ui7/pages/call_page.dart';
+import 'package:my_project/practice/Ui7/pages/chat_page.dart';
+import 'package:my_project/practice/Ui7/pages/status_page.dart';
 
 class WhatsappListOfMapDemo extends StatelessWidget {
   const WhatsappListOfMapDemo({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(18, 140, 126, 5),
-        title: const Text("WhatsApp"),
-        actions: const [
-          SizedBox(
-            width: 120,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.camera_alt_outlined),
-                Icon(Icons.search_outlined),
-                Icon(Icons.more_vert),
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color.fromRGBO(18, 140, 126, 5),
+            title: const Text("WhatsApp"),
+            actions: const [
+              SizedBox(
+                width: 120,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.camera_alt_outlined),
+                    Icon(Icons.search_outlined),
+                    Icon(Icons.more_vert),
+                  ],
+                ),
+              ),
+            ],
+            elevation: 5,
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  text: 'chat',
+                ),
+                Tab(text: 'Status'),
+                Tab(
+                  text: 'Calls',
+                ),
               ],
             ),
           ),
-        ],
-        elevation: 5,
-        bottom: const PreferredSize(
-          preferredSize: Size(double.infinity, 60),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(
-                Icons.groups,
-                color: Colors.white70,
-                size: 27,
-              ),
-              Text(
-                "Chats",
-                style: TextStyle(color: Colors.white70, fontSize: 18),
-              ),
-              Text(
-                "Status",
-                style: TextStyle(color: Colors.white70, fontSize: 18),
-              ),
-              Text(
-                "Calls",
-                style: TextStyle(color: Colors.white70, fontSize: 18),
-              ),
-            ],
+          body: const TabBarView(
+            children: [ChatPage(), StatusPage(), CallPage()],
           ),
         ),
-      ),
-      body: Container(
-        height: 684,
-        width: double.infinity,
-        color: const Color.fromRGBO(7, 94, 84, 10),
-        child: SingleChildScrollView(
-            child: Column(
-                children: List.generate(
-          myDetails.length,
-          (index) => ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage(myDetails[index]['url']),
-              radius: 18,
-            ),
-            title: Text(myDetails[index]['name']),
-            subtitle: Text(myDetails[index]['message']),
-            trailing: Text(myDetails[index]['date']),
-          ),
-        ))),
-      ),
-    );
-  }
+      );
 }
